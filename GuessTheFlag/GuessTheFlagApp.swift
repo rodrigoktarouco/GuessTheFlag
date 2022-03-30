ccc18f3446344c0d6154ed11a5c99052e70fd104
Rodrigo Kroef tarouco <55713305+rodrigoktarouco@users.noreply.github.com>
2022-03-30 14:49:56 -0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Rodrigo Tarouco on 30/03/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
